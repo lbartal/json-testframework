7ec1e885bdd4fc7ed4e997fb8d519942b1493d5e
lbartal <laszlo.bartal@gmail.com>
2015-09-20 17:53:11 +0200

    mavenize

---
+++ src/main/java/com/lbartal/app/jsontestframework/App.java
@@ -0,0 +1,13 @@
+package com.lbartal.app.jsontestframework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
